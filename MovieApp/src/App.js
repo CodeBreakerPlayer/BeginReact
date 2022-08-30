2f05e9a1b371eaf40d37cffde67ec5ef92d9cd66
CodeBreakerPalyer <>
2022-08-30 20:07:43 +0900

    MovieApp - useParameter 실습

--- MovieApp/src/App.js
+++ MovieApp/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/hello" element={<h1>Hello</h1>}></Route>
-        <Route path="/movie" element={<Detail />}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
