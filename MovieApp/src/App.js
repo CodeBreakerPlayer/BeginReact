ebbdc741631cfd57841b2f89baf4c541a448f0c5
CodeBreakerPalyer <>
2022-08-30 19:48:57 +0900

    MovieApp - Router 실습

---
+++ MovieApp/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>}></Route>
+        <Route path="/movie" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
