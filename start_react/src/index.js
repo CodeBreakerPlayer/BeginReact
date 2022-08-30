15b4a6781f3b288ca17749dd5a6a7b811f929c80
CodeBreakerPalyer <>
2022-08-30 18:23:54 +0900

    React Todo 실습

--- start_react/src/index.js
+++ start_react/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import AppCleanUp from "./AppCleanUp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppCleanUp />);
+root.render(<App />);
